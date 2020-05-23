a518b6eeff33c7079917e6ccc57f55faf65e68a4
catsymptote <catsymptote@gmail.com>
2020-05-24 00:29:00 +0200

    Update speed_optimiser.py

--- speed_optimiser.py
+++ speed_optimiser.py
@@ -4,23 +4,24 @@ from science.hamming_weight import *
 import time
 
 
-runs = 1000
+runs = 10000
+key_size = 8192
 
 crypt = Crypto_Tools()
 data = 'cryptography'
 keys = []
 for i in range(runs):
-    keys.append(get_random_binary(128))
+    keys.append(get_random_binary(key_size))
 
 #word_list = crypt.get_QR(keys[0], data)
-
+print("Construction done!")
 ###################
-start = time.time()
+start_time = time.time()
 ###################
 
 
 for i in range(runs):
-    crypt.get_QR(keys[0], data)
+    crypt.get_QR(keys[i], data)
     #crypt.get_QR(keys[i], data)
     #crypt.gen_QR(keys[i], data)
     #tmp = crypt.word_list_to_bits(word_list)
@@ -28,7 +29,10 @@ for i in range(runs):
 
 
 ################################
-end = (time.time() - start)/runs
+end_time = time.time() - start_time#/runs
 ################################
 
-print('\n', end, '\n')
+print('\n', key_size, end_time, '\n')
+#print(end_time)
+#print(start_time)
+#print(time.time())
